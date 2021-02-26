94c494c09cb7c67d21528cc772c97d14c6bc67cd
JunMock <kjmzzang01@gmail.com>
2021-02-26 23:22:02 +0900

    baekjoon 5545 최고의 피자 문제 완료

--- baekjoon_5545_최고의피자/Main.java
+++ baekjoon_5545_최고의피자/Main.java
@@ -2,34 +2,46 @@ package baekjoon_5545_최고의피자;
 
 import java.io.*;
 import java.util.*;
-// 
+
+// 14360KB 128ms
 public class Main {
-   
+    static int N, A, B, C, S;
+    static Integer[] arr;
+
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int N = Integer.parseInt(br.readLine());
-
-        String[] arr = new String[N];
-
+        N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        A = Integer.parseInt(st.nextToken());
+        B = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(br.readLine());
+        arr = new Integer[N];
+        S = C / A;
         for (int i = 0; i < N; i++) {
-            arr[i] = br.readLine();
+            arr[i] = Integer.parseInt(br.readLine());
         }
-
+        Arrays.sort(arr, Collections.reverseOrder());
         br.close();
-
+        
+        Solution();
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        for (int i = 0; i < N; i++) {
-            bw.write(arr[i], 0, arr[i].length());
-            bw.newLine();
-        }
+        bw.write(String.valueOf(S));
 
         bw.flush();
         bw.close();
     }
 
-    
+    public static void Solution() {
+        for (int i = 0; i < N; i++) {
+            if (S <= (C + arr[i]) / (A + B)) {
+                S = (C + arr[i]) / (A + B);
+                C = C + arr[i];
+                A = A + B;
+            } else
+                break;
+        }
 
+    }
 }
