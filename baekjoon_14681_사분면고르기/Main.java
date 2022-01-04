df218411a396e3fb34396db12b90dba9c7bb4f39
JunMock <kjmzzang01@gmail.com>
2022-01-04 22:30:31 +0900

    boj 14681 sol

---
+++ baekjoon_14681_사분면고르기/Main.java
@@ -0,0 +1,32 @@
+package baekjoon_14681_사분면고르기;
+
+import java.io.*;
+import java.util.*;
+
+// 14204 KB 132 MS
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int x = Integer.parseInt(br.readLine());
+        int y = Integer.parseInt(br.readLine());
+        int a = 0;
+        if (x > 0) {
+            if (y < 0)
+                a = 4;
+            else
+                a = 1;
+        } else {
+            if (y < 0)
+                a = 3;
+            else
+                a = 2;
+        }
+        bw.write(Integer.toString(a));
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+
+}
